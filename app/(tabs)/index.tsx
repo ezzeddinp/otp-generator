40f1e25c8c82f85d064082de1a2a6df08880ef5a
ezzeddinPratama <ezzeddinpratama04@gmail.com>
2024-07-08 13:05:42 +0700

    react native exercise 3 - Simple Otp Generator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,153 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const OtpGenerator: React.FC = () => {
+	const [otp, setOtp] = useState<string>('');
+	const [userInput, setUserInput] = useState<string>('');
+	const [isValid, setIsValid] = useState<boolean | null>(null); // bisa aja user ga input
+	const [showOtpBox, setShowOtpBox] = useState<boolean>(false)
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const generateOtp = () => {
+		let generatedOtp = ''; // pake let biar nilainya changeable
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+		for (let i = 0; i < 6; i++) {
+			generatedOtp += characters.charAt(Math.floor(Math.random() * characters.length));
+		}
+
+		setOtp(generatedOtp);
+		setIsValid(null); // karna blm di cek, baru digenerate doang
+		setShowOtpBox(true)
+	}
+
+	const validateOtp = () => {
+		if (userInput === otp) {
+			setIsValid(true);
+		} else {
+			setIsValid(false);
+		}
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.box}>
+				<Text style={styles.title}>
+					Simple OTP Generator | Validator
+				</Text>
+				<TouchableOpacity style={styles.button} onPress={generateOtp}>
+					<Text style={styles.buttonText}>
+						Generate OTP
+					</Text>
+				</TouchableOpacity>
+				{
+					showOtpBox && (
+						<View style={styles.otpBox}>
+							<Text style={styles.otp}>
+								{otp}
+							</Text>
+						</View>
+					)
+				}
+				<TextInput
+					style={styles.input}
+					placeholder="Enter OTP"
+					value={userInput}
+					onChangeText={setUserInput}
+				/>
+				<TouchableOpacity style={styles.button} onPress={validateOtp}>
+					<Text style={styles.buttonText}>
+						Validate OTP
+					</Text>
+				</TouchableOpacity>
+				{
+					isValid === true && (
+						<Text style={styles.validText}>
+							OTP is valid
+						</Text>
+					)
+				}
+				{
+					isValid === false && (
+						<Text style={styles.invalidText}>
+							OTP is invalid
+						</Text>
+					)
+				}
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'black'
+	},
+	box: {
+		width: '80%',
+		backgroundColor: 'black',
+		borderRadius: 10,
+		padding: 20,
+		shadowColor: '#FFF',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	title: {
+		fontSize: 24,
+		marginBottom: 20,
+		color: 'white'
+	},
+	button: {
+		backgroundColor: 'white',
+		paddingHorizontal: 30,
+		paddingVertical: 15,
+		borderRadius: 5,
+		marginTop: 20,
+	},
+	buttonText: {
+		color: 'black',
+		fontSize: 18,
+		fontWeight: '500'
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: '#007AFF',
+		borderRadius: 5,
+		color: 'white',
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		marginTop: 20,
+		width: '100%',
+	},
+	otpBox: {
+		marginTop: 20,
+		backgroundColor: 'black',
+		borderRadius: 5,
+		padding: 10,
+		borderWidth: 2,
+		borderColor: 'green',
+	},
+	otp: {
+		fontSize: 24,
+		color: 'white'
+	},
+	validText: {
+		fontSize: 20,
+		color: 'green',
+		marginTop: 20,
+	},
+	invalidText: {
+		fontSize: 20,
+		color: 'red',
+		marginTop: 20,
+	},
 });
+
+export default OtpGenerator
